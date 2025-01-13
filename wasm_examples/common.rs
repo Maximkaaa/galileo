d2743a6c5b2ac5e06a6c2c6ea092662a005a8cca
Maxim <maxim@gritsenko.biz>
2025-01-13 15:55:29 +0900

    Update winit in web examples
    This commit brings the web examples up to day with the recent update of
    winit. All examples seem to work fine except egui and vector tile ones,
    but I'll deal with them in separate PRs.

--- wasm_examples/common.rs
+++ wasm_examples/common.rs
@@ -1,47 +1,27 @@
-use winit::dpi::PhysicalSize;
-use winit::event_loop::EventLoop;
-use winit::platform::web::WindowExtWebSys;
-use winit::window::{Window, WindowBuilder};
+use galileo_types::cartesian::Size;
+use web_sys::HtmlElement;
+use wasm_bindgen::JsCast;
 
-pub async fn set_up() -> (Window, EventLoop<()>) {
-    let event_loop = EventLoop::new().unwrap();
-    let window = WindowBuilder::new().build(&event_loop).unwrap();
-    let window = window;
-
-    web_sys::window()
-        .and_then(|win| win.document())
-        .and_then(|doc| {
-            let dst = doc.get_element_by_id("map")?;
-            let canvas = web_sys::Element::from(window.canvas()?);
-            dst.append_child(&canvas).ok()?;
-
-            Some(())
-        })
-        .expect("Couldn't create canvas");
+pub async fn set_up() -> (HtmlElement, Size<u32>) {
+    console_log::init_with_level(log::Level::Info).expect("Couldn't init logger");
 
     let web_window = web_sys::window().unwrap();
+    let container: HtmlElement = web_window.document()
+        .and_then(|doc| {
+            doc.get_element_by_id("map")
+        })
+        .expect("Coundn't get map container")
+        .dyn_into()
+        .expect("Container is not an HtmlElement");
     let scale = web_window.device_pixel_ratio();
 
-    let _ = window.request_inner_size(PhysicalSize::new(
-        web_window.inner_width().unwrap().as_f64().unwrap() * scale,
-        web_window.inner_height().unwrap().as_f64().unwrap() * scale,
-    ));
+    let width = web_window.inner_width().unwrap().as_f64().unwrap() * scale;
+    let height = web_window.inner_height().unwrap().as_f64().unwrap() * scale;
 
-    sleep(10).await;
-    log::info!("Canvas size: {:?}", window.inner_size());
+    let size = Size::new(width as u32, height as u32);
 
-    (window, event_loop)
+    log::info!("Window size is {size:?}");
+
+    (container, size)
 }
 
-async fn sleep(duration: i32) {
-    let mut cb = |resolve: js_sys::Function, _reject: js_sys::Function| {
-        web_sys::window()
-            .unwrap()
-            .set_timeout_with_callback_and_timeout_and_arguments_0(&resolve, duration)
-            .unwrap();
-    };
-
-    let p = js_sys::Promise::new(&mut cb);
-
-    wasm_bindgen_futures::JsFuture::from(p).await.unwrap();
-}
