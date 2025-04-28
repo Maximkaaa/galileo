836517f25f4b529c67b630e9d6cb8f9dc673ec4f
Erik Lee <2141800+aethertap@users.noreply.github.com>
2025-04-28 06:54:06 -0500

    Updated quick-start with newer version of MapBuilder API

--- galileo/src/lib.rs
+++ galileo/src/lib.rs
@@ -20,12 +20,15 @@
 //! **src/main.rs:**
 //! ```rust
 //! use galileo::layer::raster_tile_layer::RasterTileLayerBuilder;
-//! use galileo::{Map, MapBuilder, TileSchema };
+//! use galileo::{Map, MapBuilder};
 //! use galileo::layer::FeatureLayer;
 //! use galileo::symbol::CirclePointSymbol;
 //! use galileo::galileo_types::latlon;
 //! use galileo_types::geo::Crs;
 //! use galileo::Color;
+//! 
+//! mod app;
+//! use app::MapApp;
 //! 
 //! fn main() {
 //!     run()
@@ -33,7 +36,7 @@
 //! 
 //! // Creates the window and starts the egui event loop.
 //! pub(crate) fn run() {
-//!     galileo_egui::init(create_map(), [])
+//!     galileo_egui::init_with_app(Box::new(|cc| Ok(Box::new(MapApp::new(create_map(),cc,[])))))
 //!         .expect("Couldn't create window");
 //! }
 //! 
@@ -41,10 +44,9 @@
 //! fn create_map()->Map {
 //!     MapBuilder::default()
 //!         // Set the position of the center of the map view
-//!         .with_position(latlon!(37.566, 126.9784))
-//!         // Set the size of the smallest visible feature (zoom level) for the view to start
-//!         // with.
-//!         .with_resolution(TileSchema::web(18).lod_resolution(8).unwrap())
+//!         .with_latlon(37.566, 126.9784)
+//!         // Set the size of the smallest visible feature (zoom level) for the view to start with.
+//!         .with_z_level(8)
 //!         // Add the Open Street Maps raster tile server as a layer
 //!         .with_layer(RasterTileLayerBuilder::new_osm().build().unwrap())
 //!         // Add a blue dot at the coordinates specified
@@ -53,8 +55,7 @@
 //!             vec![latlon!(37.566, 126.9784)], 
 //!             // a blue circle with fixed size of 5.0 pixels
 //!             CirclePointSymbol::new(Color::BLUE, 5.0), 
-//!             // WGS84 is the coordinate reference system that's based on latitude/longitude
-//!             // pairs.
+//!             // WGS84 is the coordinate reference system that's based on latitude/longitude pairs.
 //!             Crs::WGS84, 
 //!         ))
 //!         .build()
