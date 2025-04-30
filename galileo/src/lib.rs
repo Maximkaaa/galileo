dfe8291a14f2c62480a280a99075d8303593d84d
Erik Lee <2141800+aethertap@users.noreply.github.com>
2025-04-30 07:09:15 -0500

    Add updated quick start example in the docs

--- galileo/src/lib.rs
+++ galileo/src/lib.rs
@@ -3,7 +3,60 @@
 //!
 //! # Quick start
 //!
-//! TODO
+//! **Cargo.toml:**
+//! ```toml
+//! [package]
+//! name = "map-view"
+//! version = "0.1.0"
+//! edition = "2024"
+//!
+//! [dependencies]
+//! galileo = {git = "https://github.com/maximkaaa/galileo"}
+//! galileo-types = {git = "https://github.com/maximkaaa/galileo"}
+//! galileo-egui = {git = "https://github.com/maximkaaa/galileo"}
+//! ```
+//!
+//! **src/main.rs:**
+//! ```no_run
+//! use galileo::layer::raster_tile_layer::RasterTileLayerBuilder;
+//! use galileo::{Map, MapBuilder};
+//! use galileo::layer::FeatureLayer;
+//! use galileo::symbol::CirclePointSymbol;
+//! use galileo::galileo_types::latlon;
+//! use galileo_types::geo::Crs;
+//! use galileo::Color;
+//!
+//! fn main() {
+//!     run()
+//! }
+//!
+//! // Creates the window and starts the egui event loop.
+//! pub(crate) fn run() {
+//!     galileo_egui::init(create_map(), [])
+//!         .expect("Couldn't create window");
+//! }
+//!
+//! // build a map showing the Open Street Map raster data for Seoul, South Korea
+//! fn create_map()->Map {
+//!     MapBuilder::default()
+//!         // Set the position of the center of the map view
+//!         .with_latlon(37.566, 126.9784)
+//!         // Set the size of the smallest visible feature (zoom level) for the view to start with.
+//!         .with_z_level(8)
+//!         // Add the Open Street Maps raster tile server as a layer
+//!         .with_layer(RasterTileLayerBuilder::new_osm().build().unwrap())
+//!         // Add a blue dot at the coordinates specified
+//!         .with_layer(FeatureLayer::new(
+//!             // the position of the marker we're going to add, in the WGS84 CRS
+//!             vec![latlon!(37.566, 126.9784)],
+//!             // a blue circle with fixed size of 5.0 pixels
+//!             CirclePointSymbol::new(Color::BLUE, 5.0),
+//!             // WGS84 is the coordinate reference system that's based on latitude/longitude pairs.
+//!             Crs::WGS84,
+//!         ))
+//!         .build()
+//! }
+//! ```
 //!
 //! # Main components of Galileo
 //!
