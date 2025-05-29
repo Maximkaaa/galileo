8f479caa0c95686b7f83854efd9543f6c908b18f
Maxim <maxim@gritsenko.biz>
2025-05-29 13:00:55 +0900

    Simplity geojson module implementation

--- galileo-types/src/geojson/mod.rs
+++ galileo-types/src/geojson/mod.rs
@@ -1,21 +1,35 @@
 use geojson::{LineStringType, PolygonType, Position, Value};
 
+use crate::error::GalileoTypesError;
+use crate::geo::impls::GeoPoint2d;
+use crate::geo::NewGeoPoint;
 use crate::geo::Projection;
-use crate::geojson::point::GeoJsonPoint;
 use crate::geometry::{Geom, Geometry};
 use crate::impls::{Contour, MultiContour, MultiPoint, MultiPolygon, Polygon};
 
-mod point;
+impl TryFrom<Position> for GeoPoint2d {
+    type Error = GalileoTypesError;
+
+    fn try_from(value: Position) -> Result<Self, Self::Error> {
+        if value.len() < 2 {
+            Err(GalileoTypesError::Conversion(
+                "point must contain at least 2 dimensions".to_string(),
+            ))
+        } else {
+            Ok(Self::latlon(value[1], value[0]))
+        }
+    }
+}
 
 impl Geometry for geojson::Geometry {
-    type Point = GeoJsonPoint;
+    type Point = GeoPoint2d;
 
     fn project<Proj>(&self, projection: &Proj) -> Option<Geom<Proj::OutPoint>>
     where
         Proj: Projection<InPoint = Self::Point> + ?Sized,
     {
         match &self.value {
-            Value::Point(p) => GeoJsonPoint::try_from(p.clone()).ok()?.project(projection),
+            Value::Point(p) => GeoPoint2d::try_from(p.clone()).ok()?.project(projection),
             Value::MultiPoint(points) => convert_multi_point(points)?.project(projection),
             Value::LineString(points) => convert_contour(points)?.project(projection),
             Value::MultiLineString(lines) => convert_multi_contour(lines)?.project(projection),
@@ -26,27 +40,27 @@ impl Geometry for geojson::Geometry {
     }
 }
 
-fn convert_contour(line_string: &LineStringType) -> Option<Contour<GeoJsonPoint>> {
+fn convert_contour(line_string: &LineStringType) -> Option<Contour<GeoPoint2d>> {
     let is_closed = !line_string.is_empty() && line_string[0] == line_string[line_string.len() - 1];
     Some(Contour::new(
         line_string
             .iter()
-            .map(|p| GeoJsonPoint::try_from(p.clone()).ok())
+            .map(|p| GeoPoint2d::try_from(p.clone()).ok())
             .collect::<Option<Vec<_>>>()?,
         is_closed,
     ))
 }
 
-fn convert_multi_point(points: &[Position]) -> Option<MultiPoint<GeoJsonPoint>> {
+fn convert_multi_point(points: &[Position]) -> Option<MultiPoint<GeoPoint2d>> {
     Some(MultiPoint::from(
         points
             .iter()
-            .map(|p| GeoJsonPoint::try_from(p.clone()).ok())
+            .map(|p| GeoPoint2d::try_from(p.clone()).ok())
             .collect::<Option<Vec<_>>>()?,
     ))
 }
 
-fn convert_multi_contour(lines: &[LineStringType]) -> Option<MultiContour<GeoJsonPoint>> {
+fn convert_multi_contour(lines: &[LineStringType]) -> Option<MultiContour<GeoPoint2d>> {
     Some(MultiContour::from(
         lines
             .iter()
@@ -55,7 +69,7 @@ fn convert_multi_contour(lines: &[LineStringType]) -> Option<MultiContour<GeoJso
     ))
 }
 
-fn convert_polygon(polygon: &PolygonType) -> Option<Polygon<GeoJsonPoint>> {
+fn convert_polygon(polygon: &PolygonType) -> Option<Polygon<GeoPoint2d>> {
     Some(Polygon::new(
         convert_contour(&polygon[0])?.into_closed()?,
         polygon[1..]
@@ -65,7 +79,7 @@ fn convert_polygon(polygon: &PolygonType) -> Option<Polygon<GeoJsonPoint>> {
     ))
 }
 
-fn convert_multi_polygon(mp: &[PolygonType]) -> Option<MultiPolygon<GeoJsonPoint>> {
+fn convert_multi_polygon(mp: &[PolygonType]) -> Option<MultiPolygon<GeoPoint2d>> {
     Some(MultiPolygon::from(
         mp.iter().map(convert_polygon).collect::<Option<Vec<_>>>()?,
     ))
